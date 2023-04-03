40534efe5e426bab4c171c76062756190fad5115
1salo <william-salo@hotmail.com>
2023-04-03 13:59:34 +0200

    folders, nav, footer

---
+++ src/Navigation/Footer.tsx
@@ -0,0 +1,68 @@
+// import "./Navigation.css";
+// import Logo from "../assets/logo.png";
+// // import { Link } from "react-router-dom";
+
+// const Footer = () => {
+//   return (
+//     <>
+//       <footer>
+//         <div className="footern">
+//           <ul className="footer-links">
+//             <li>
+//               <h4>Här finns vi</h4>
+//               <a
+//                 href="https://www.betapedagog.se/pub_images/large/12828_2775_2.jpg"
+//                 target="_blank"
+//               >
+//                 <p>Sverige</p>
+//               </a>
+//               <a
+//                 href="https://static.wikia.nocookie.net/marvelcinematicuniverse/images/5/56/Map_of_Wakanda.png/revision/latest?cb=20211017005219"
+//                 target="_blank"
+//               >
+//                 <p>Wakanda</p>
+//               </a>
+//               <a
+//                 href="https://cdn.abicart.com/shop/27062/art62/h9591/182409591-origpic-e3f7a0.jpg"
+//                 target="_blank"
+//               >
+//                 <p>Hogwarts</p>
+//               </a>
+//             </li>
+//             <li>
+//               <h4>Mina sidor</h4>
+//               <a href="/minaSidor.html">
+//                 <p>Logga in</p>
+//               </a>
+//               <a href="/minaSidor.html">
+//                 <p>Skapa konto</p>
+//               </a>
+//               <Link className="routeText" to="myBookings">
+//                 Bokningar
+//               </Link>
+//             </li>
+//             <li>
+//               <h4>Kontakt</h4>
+//               <a href="tel:070 123 45 67">
+//                 <p>070 123 45 67</p>
+//               </a>
+//               <a href="mailto:info@städarfintsomfan">
+//                 <p>info@städafint.nu</p>
+//               </a>
+//               <a href="#">
+//                 <p>Kontakta oss</p>
+//               </a>
+//             </li>
+//           </ul>
+//           <div className="logotitel2">
+//             <Link to="start">
+//               <img id="nav__logo" src={Logo} alt="stadafint-logo" />
+//             </Link>
+//           </div>
+//         </div>
+//       </footer>
+//     </>
+//   );
+// };
+
+// export default Footer;
---
+++ src/Navigation/Nav.tsx
@@ -0,0 +1,43 @@
+// import "./Navigation.css";
+
+// // Image imorts
+// import Logo from "../assets/logo.png";
+// import { Link } from "react-router-dom";
+
+// const Nav = () => {
+//   return (
+//     <nav>
+//       <div className="logotitel">
+//         <Link to="start">
+//           <img id="nav__logo" src={Logo} alt="stadafint-logo" />
+//         </Link>
+//       </div>
+//       <div>
+//         <ul className="nav-links">
+//           <li>
+//             <Link to="start">Start</Link>
+//           </li>
+//           <li>
+//             <a href="">Våra tjänster</a>
+//           </li>
+//           <li>
+//             <a href="/minaSidorForetag.html">Företag</a>
+//           </li>
+//           <li>
+//             <a
+//               href="https://www.youtube.com/watch?v=YF1R0hc5Q2I&ab_channel=FleetwoodMac"
+//               target="_blank"
+//             >
+//               Här finns vi
+//             </a>
+//           </li>
+//           <li>
+//             <Link to="myBookings">Mina bokningar</Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Nav;
